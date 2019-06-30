a44a1128310cd26b0de4e1d7d150d81984487b40
Riad Hossain <riadmahmudeng@gmail.com>
2019-07-01 00:08:53 +0600

    update
    update

--- Login.php
+++ Login.php
@@ -24,13 +24,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
      }
   ?>
             <form action="" method="POST">
-                <div class="form-group">
+                <div class="form-group dihedear">
+                    <label for="username">User Name</label>
+                    <input type="text" id="username" name="username" class="form-control"  />
+                </div>
+                <div class="form-group dihedear">
                     <label for="email">Email Address</label>
                     <input type="text" id="email" name="email" class="form-control"  />
                 </div>
                 <div class="form-group">
                     <label for="password">Password</label>
                     <input type="password" id="password" name="password" class="form-control"  />
+                </div>
+                   <div class="form-group">
+                    <label for="cpassword">Conform Password</label>
+                    <input type="password" id="cpassword" name="cpassword" class="form-control"  />
                 </div>
                 <button type="submit" name="login" class="btn btn-success">Login</button>
             </form>
